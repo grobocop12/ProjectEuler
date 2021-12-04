b05f905e89655666241ff93bcf438d3f732ef4ef
grobocop12 <chasnik97876@gmail.com>
2021-12-04 18:14:26 +0100

    problem 1 test

--- src/test/scala/com/grobocop/euler/problem1/MultiplesOf3Or5Spec.scala
+++ src/test/scala/com/grobocop/euler/problem1/MultiplesOf3Or5Spec.scala
@@ -1,5 +1,19 @@
 package com.grobocop.euler.problem1
 
-class MultiplesOf3Or5Spec {
+import org.scalatest.flatspec.AnyFlatSpec
 
+class MultiplesOf3Or5Spec extends AnyFlatSpec {
+  behavior of "MultiplesOf3Or5"
+
+  it should "return 23 when passed numbers 1 to 9" in {
+    val result = MultiplesOf3Or5(1L to 9)
+
+    assert(result === 23)
+  }
+
+  it should "return 233168 when passed numbers 1 to 999" in {
+    val result = MultiplesOf3Or5(1L to 999)
+
+    assert(result === 233168)
+  }
 }

ec339893127e1949f4a39e91ec6b6a9deaa127d3
grobocop12 <chasnik97876@gmail.com>
2022-10-16 19:44:29 +0200

    problem 12 solved

--- src/test/scala/com/grobocop/euler/problem11/LargestProductInGridSpec.scala
+++ src/test/scala/com/grobocop/euler/problem11/LargestProductInGridSpec.scala
@@ -3,7 +3,7 @@ package com.grobocop.euler.problem11
 import org.scalatest.flatspec.AnyFlatSpec
 
 class LargestProductInGridSpec extends AnyFlatSpec {
-  behavior of "SpecialPythagoreanTriplet"
+  behavior of "LargestProductInGrid"
 
   it should "return 70600674" in {
     val result = LargestProductInGrid()
